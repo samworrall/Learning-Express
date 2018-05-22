884db437b0e38352190c3dabe391547afb3af45f
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-22 13:24:12 +0100

    Homepage displays user entered name

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 //Require express and make it an object
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 //Represent our app as an express object
 const app = express();
 //Tell the app to listen to a port
+const upload = multer();
 
 app.set('view engine', 'pug');
+app.use(upload.array());
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
@@ -12,5 +19,14 @@ app.listen(3000, function() {
 
 //Two arguments, the path, and a callback function which has two arguments, req and res
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {retrievedNames : app.get('data')});
+});
+
+app.get('/new_name', function(req, res) {
+  res.render('newName');
+});
+
+app.post('/new_name', function(req, res) {
+  app.set('data', req.body.name);
+  res.redirect('/')
 });
--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,6 +1,23 @@
 describe('App', function() {
-  it('Should display Hello World!', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000/');
-    cy.contains('Hello Pug Test').should('be.visible');
+  });
+
+  it('Should display Hello World!', function() {
+    cy.contains('List of Names').should('be.visible');
+  });
+
+  it('Should display a name form', function() {
+    cy.get('form').find('input').contains('Add name').click();
+    cy.contains('Enter Name:').should('be.visible');
+  });
+
+  it('Should update the displayed name', function() {
+    cy.get('form').find('input').contains('Add name').click();
+    cy.get('.text')
+      .type('Sam')
+      .should('have.value', 'Sam')
+    cy.get('form').find('input').contains('Add name').click();
+    cy.contains('Sam').should('be.visible');
   });
 });
