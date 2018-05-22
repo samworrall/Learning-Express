469e21c6f0eaeae7d968534d494143e6fef8df6e
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-05-22 15:20:20 +0100

    Use express.urlencoded over body-parser

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-//Require express and make it an object
 const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-//Represent our app as an express object
 const app = express();
-//Tell the app to listen to a port
-const upload = multer();
 
 app.set('view engine', 'pug');
-app.use(upload.array());
-app.use(express.static('public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, function() {
   console.log('Listening on port 3000');
 });
 
+var names = []
 //Two arguments, the path, and a callback function which has two arguments, req and res
 app.get('/', function(req, res) {
-  res.render('index', {retrievedNames : app.get('data')});
+  res.render('index', {retrievedNames : names});
 });
 
 app.get('/new_name', function(req, res) {
@@ -27,6 +19,6 @@ app.get('/new_name', function(req, res) {
 });
 
 app.post('/new_name', function(req, res) {
-  app.set('data', req.body.name);
+  names.push(req.body.name);
   res.redirect('/')
 });
